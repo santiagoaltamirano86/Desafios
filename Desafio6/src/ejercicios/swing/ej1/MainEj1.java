578dd71708b94fcdbb1996f99f53df762041bbf7
santi <santi@SANTIAGOPC>
2025-11-15 10:02:33 -0300

    Agrego ejercicios Swing Desafio6

---
+++ Desafio6/src/ejercicios/swing/ej1/MainEj1.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicios.swing.ej1;
+
+/**
+ *
+ * @author santi
+ */
+public class MainEj1 {
+    
+}
